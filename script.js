c04e47cc4f06b22982c89fa38001cb1494402a86
smm <stepan.mishura@gmail.com>
2024-09-28 00:39:26 +0700

    some random state

---
+++ script.js
@@ -0,0 +1,19 @@
+
+/**
+let tg = window.Telegram.WebApp;
+
+window.alert(window.innerWidth);
+window.alert(window.innerHeight);
+
+const c = document.getElementById("myCanvas");
+const ctx = c.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(250, 20, 300, 100);
+ctx.stroke();
+ctx.fillStyle = "red";
+ctx.fill();
+*/
+
+window.alert(window.screen.width);
+window.alert(window.screen.height);
