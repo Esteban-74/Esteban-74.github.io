3cf1239168793b68a1373e71efccd60bd70efc7e
smm <stepan.mishura@gmail.com>
2024-09-28 10:16:24 +0700

    try to resize chess board

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ let board = document.getElementById('board')
 //window.alert(window.screen.width);
 //window.alert(window.screen.height);
 
+var w = window.innerWidth;
+var h = window.innerHeight;
+
 let rows_num = 8;
+
+let field_size = w;
+if (w > h) {
+    field_size = h;
+}
+
+let row_size_str = (field_size / rows_num)  + "px";
+
 for(let i = 1; i <= rows_num; i++){
     const cellContainer = document.createElement("div");
     cellContainer.className = "cellContainer";
-    for( let j = 0; j<=rows_num; j++ ){
+    for( let j = 1; j<=rows_num; j++ ){
         const cell = document.createElement("div");
         cell.className = "cell";
+        cell.style.width = row_size_str;
+        cell.style.height = row_size_str;
         cellContainer.append(cell); 
     }
 
