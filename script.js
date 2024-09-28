aeb1a10a486d3811344172e1d17f8cbfe77887c8
smm <stepan.mishura@gmail.com>
2024-09-28 12:45:45 +0700

    try to turn on web_app_data

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ if (w > h) {
     field_size = h;
 }
 
-let row_size_str = (field_size / rows_num)  + "px";
+let row_size = field_size / rows_num;
+let row_size_str = row_size + "px";
+let checker_size_str = (row_size * 9)/10 + "px"
 
 for(let i = 1; i <= rows_num; i++){
     const cellContainer = document.createElement("div");
@@ -24,8 +26,36 @@ for(let i = 1; i <= rows_num; i++){
         cell.className = "cell";
         cell.style.width = row_size_str;
         cell.style.height = row_size_str;
+
+        cell.style.backgroundColor = "white";
+        if ( ((i+j)%2) > 0 ) {
+            cell.style.backgroundColor = "black";
+        }
+        if ((i<4) && (((i+j)%2) > 0)){
+            const checker = document.createElement("div");
+            checker.className = "checker";
+            checker.style.width = checker_size_str;
+            checker.style.height = checker_size_str;
+            checker.style.backgroundColor = "green";
+
+            checker.onclick = 'updateColor()';
+
+            cell.append(checker);
+
+        }
         cellContainer.append(cell); 
+
+
     }
 
     board.append(cellContainer);
 }
+
+let tg = window.Telegram.WebApp;
+tg.sendData("some string that we need to send");
+
+/*
+function updateColor(){
+
+}
+*/
