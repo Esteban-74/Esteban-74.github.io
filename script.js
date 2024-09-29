7c7a53f7f3f7ce8159fe60a8976ea3fcb6744056
smm <stepan.mishura@gmail.com>
2024-09-29 12:20:39 +0700

    introduce cell_black.with_pawn_black::before

--- script.js
+++ script.js
@@ -25,42 +25,27 @@ for(let i = 1; i <= rows_num; i++){
     cellContainer.className = "cellContainer";
     for( let j = 1; j<=rows_num; j++ ){
         const cell = document.createElement("div");
-        cell.className = "cell";
         cell.style.width = row_size_str;
         cell.style.height = row_size_str;
 
-        cell.style.backgroundColor = "white";
         if ( ((i+j)%2) > 0 ) {
-            cell.style.backgroundColor = "black";
+            cell.className = "cell_white";
+        } else {
+            if (i>5) {
+                cell.className = "cell_black with_pawn_white";
+            } else if (i<4){
+                cell.className = "cell_black with_pawn_black";
+            }else {
+                cell.className = "cell_black";
+            }
+            //cell.addEventListener("click", function (){
+            //    data_str = `string: ${i}, ${j}`;
+            //});
         }
-    
-        if ((i<4) && (((i+j)%2) > 0)){
-            const checker = document.createElement("div");
-            checker.className = "checker";
-            checker.style.width = checker_size_str;
-            checker.style.height = checker_size_str;
-            checker.style.backgroundColor = "green";
-
-            //checker.onclick = `updateColor(${i}, ${j})`;
-            //checker.onclick = "'updateColor(" + i + "," + j +")'";
-            //checker.onclick = "updateColor();";
-
-            //checker.addEventListener("click", updateColor(i,j));
-            checker.addEventListener("click", function (){
-                data_str = `string: ${i}, ${j}`;
-                //window.alert(data_str);
-                tg.sendData(data_str);
-            });
-
-            cell.append(checker);
-
-        }
-    
         cellContainer.append(cell); 
 
 
     }
-
     board.append(cellContainer);
 }
 
